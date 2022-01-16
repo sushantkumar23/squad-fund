e4e5adc798621e0ddf2da6085c73758a06cf883c
Sushant Kumar <sushantkumar23@gmail.com>
2022-01-16 18:52:28 +0530

    Added the squad-fund logo

--- components/layout.js
+++ components/layout.js
@@ -1,9 +1,12 @@
 import { Fragment } from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
 import { Popover, Transition } from '@headlessui/react'
 import {
   MenuIcon,
   XIcon,
 } from '@heroicons/react/outline'
+import logo from '../public/logo.png'
 
 const footerNavigation = {
   social: [
@@ -78,14 +81,17 @@ export default function Layout({ children }) {
         <Popover className="relative bg-white">
           <div className="flex justify-between items-center max-w-7xl mx-auto px-4 py-6 sm:px-6 md:justify-start md:space-x-10 lg:px-8">
             <div className="flex justify-start lg:w-0 lg:flex-1">
-              <a href="#">
-                <span className="sr-only">Workflow</span>
-                <img
-                  className="h-8 w-auto sm:h-10"
-                  src="https://tailwindui.com/img/logos/workflow-mark-purple-600-to-indigo-600.svg"
-                  alt=""
-                />
-              </a>
+              <Link href="/" passHref>
+                <a>
+                  <span className="sr-only">Workflow</span>
+                  <Image
+                    src={logo}
+                    width={140}
+                    height={36}
+                    alt="Squad Fund logo"
+                  />
+                </a>
+              </Link>
             </div>
             <div className="-mr-2 -my-2 md:hidden">
               <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
@@ -119,13 +125,6 @@ export default function Layout({ children }) {
               <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
                 <div className="pt-5 pb-6 px-5">
                   <div className="flex items-center justify-between">
-                    <div>
-                      <img
-                        className="h-8 w-auto"
-                        src="https://tailwindui.com/img/logos/workflow-mark-purple-600-to-indigo-600.svg"
-                        alt="Workflow"
-                      />
-                    </div>
                     <div className="-mr-2">
                       <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                         <span className="sr-only">Close menu</span>
